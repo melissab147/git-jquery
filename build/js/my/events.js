1c80b06a9ad95e14f81db2b56f982a290a5ac1cc
melissab147 <melissa.j.bica@gmail.com>
2015-02-18 17:57:38 -0700

    build

--- build/js/my/events.js
+++ build/js/my/events.js
@@ -1,7 +1,24 @@
 function myEvents(){
     
-    // display a list of events belonging to me in #list
-    // e.g., https://api.github.com/users/doubleshow/events
+    console.log('listing events')    
+    
+    $.get("https://api.github.com/users/melissab147/events", github, function(data) {
 
-    alert('todo')
+        var events = data
+        var user = events[0].actor.login
+        
+        $.get("/git-jquery/templates/eventList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: events, user: user})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            myEvent()
+        })
+
+    })
+
 }
--- build/js/my/repos.js
+++ build/js/my/repos.js
@@ -1,7 +1,33 @@
 function myRepos(){
+    
+    console.log('listing repos')    
+    
+    $.get("https://api.github.com/users/melissab147/repos", github, function(data) {
 
+        // Q: What is the parameter 'github'? Where was it defined? What's the purpose? 
+        // github.js, purpose is to provide GH credentials to access API
+
+        // Q: Why is JSON.parse no longer necessary?
+        var repos = data
+        var user = repos[0].owner.login
+        
+        // Q. Why are these templates files stored in a separate folder inside contents/?
+        $.get("/git-jquery/templates/repoList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: repos, user: user})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            myRepo(repos[0].full_name)
+
+        })
+
+    })
     // display my repos in #list
     // e.g., https://api.github.com/users/doubleshow/repos
 
-    alert('todo')
+    // alert('todo')
 }
