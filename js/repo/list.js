d06245a4e4ce5dd33d0dab2b13dd637efd4d25e4
melissab147 <melissa.j.bica@gmail.com>
2015-02-18 17:57:38 -0700

    build

--- js/repo/list.js
+++ js/repo/list.js
@@ -1,4 +1,4 @@
-// Q: How is this js file loaded?
+// Q: How is this js file loaded? -From app.js
 function repoList(){
 
     console.log('listing repos')    
@@ -6,15 +6,17 @@ function repoList(){
     $.get("https://api.github.com/orgs/ucdd2-sp15/repos", github, function(data) {
 
         // Q: What is the parameter 'github'? Where was it defined? What's the purpose? 
+        // github.js, purpose is to provide GH credentials to access API
 
         // Q: Why is JSON.parse no longer necessary?
         var repos = data
+        var user = repos[0].owner.login
         
         // Q. Why are these templates files stored in a separate folder inside contents/?
         $.get("/git-jquery/templates/repoList.jade", function(template) {
 
             // render the template
-            var html = jade.render(template, {items: repos})            
+            var html = jade.render(template, {items: repos, user: user})            
 
             // assign the rendered html to the dom element whose id is #list
             $("#list").html(html)
